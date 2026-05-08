95b196f1f354d46adfea295d620135f1118a279f
StefanGeorgesco <stefan.georgesco@yahoo.fr>
2026-05-08 18:12:42 +0200

    add sec02 Lec06MonoFromCallable

---
+++ src/main/java/fr/stefangeorgesco/reactive_playground/sec02/Lec06MonoFromCallable.java
@@ -0,0 +1,25 @@
+package fr.stefangeorgesco.reactive_playground.sec02;
+
+import fr.stefangeorgesco.reactive_playground.common.Util;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+public class Lec06MonoFromCallable {
+
+    private static final Logger logger = LoggerFactory.getLogger(Lec06MonoFromCallable.class);
+
+    public static void main(String[] args) {
+        var list = List.of(1, 2, 3);
+        // Execution of sum is deferred until the subscriber subscribes
+        // Handles exceptions: the callable can throw checked exceptions
+        Mono.fromCallable(() -> sum(list)).subscribe(Util.subscriber());
+    }
+
+    private static int sum(List<Integer> list) {
+        logger.info("summing {}", list);
+        return list.stream().mapToInt(i -> i).sum();
+    }
+}

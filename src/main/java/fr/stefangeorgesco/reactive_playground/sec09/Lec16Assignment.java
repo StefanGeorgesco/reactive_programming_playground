a43ca8f5a798d2864f7bbe90967b8f0cd6e1a2bf
StefanGeorgesco <stefan.georgesco@yahoo.fr>
2026-05-31 16:43:14 +0200

    add sec09 Lec16Assignment

---
+++ src/main/java/fr/stefangeorgesco/reactive_playground/sec09/Lec16Assignment.java
@@ -0,0 +1,35 @@
+package fr.stefangeorgesco.reactive_playground.sec09;
+
+import fr.stefangeorgesco.reactive_playground.common.Util;
+import fr.stefangeorgesco.reactive_playground.sec09.applications.*;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.util.List;
+
+/*
+    Get all users and build 1 object as shown here.
+    record UserInformation(Integer userId, String username, Integer balance, List<Order> orders) {}
+ */
+public class Lec16Assignment {
+
+    record UserInformation(Integer userId, String username, Integer balance, List<Order> orders) {
+    }
+
+    public static void main(String[] args) {
+
+        UserService.getAllUsers()
+                .flatMap(Lec16Assignment::getUserInformation)
+                .subscribe(Util.subscriber("user information subscriber"));
+
+        Util.sleep(Duration.ofSeconds(2));
+    }
+
+    private static Mono<UserInformation> getUserInformation(User user) {
+        return Mono.zip(
+                        PaymentService.getUserBalance(user.id()),
+                        OrderService.getUserOrders(user.id()).collectList()
+                )
+                .map(t -> new UserInformation(user.id(), user.username(), t.getT1(), t.getT2()));
+    }
+}
